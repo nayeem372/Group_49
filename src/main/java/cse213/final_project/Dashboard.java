ea6e1e8ecae351e57c7d1e9bd46a77984400dccc
Jannatul Fardous <Jannatulferdousmobi>
2025-04-13 17:47:25 +0600

    dashboard added

---
+++ src/main/java/cse213/final_project/Dashboard.java
@@ -0,0 +1,14 @@
+package cse213.final_project;
+
+import javafx.event.ActionEvent;
+
+public class Dashboard
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void DashboardOA(ActionEvent actionEvent) {
+    }
+}
