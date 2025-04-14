87774db2b07fc20cb17e5bd533149642c0617e37
Nayeem372 <hnayeem372@gmail.com>
2025-04-14 20:01:35 +0600

    logInfxml and logInViewcontroller  created

---
+++ src/main/java/cse213/final_project/logInViewcontroller.java
@@ -0,0 +1,21 @@
+package cse213.final_project;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class logInViewcontroller
+{
+    @javafx.fxml.FXML
+    private TextField userNameTextField;
+    @javafx.fxml.FXML
+    private PasswordField passwordPasswordField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void logInOnAction(ActionEvent actionEvent) {
+    }
+}
