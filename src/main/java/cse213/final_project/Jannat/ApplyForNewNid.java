b6b58fc6e6542469fa1150f6d0e674270ffa74af
Jannatul Fardous <Jannatulferdousmobi>
2025-04-15 21:58:30 +0600

    applyNewNidAdded

---
+++ src/main/java/cse213/final_project/Jannat/ApplyForNewNid.java
@@ -0,0 +1,22 @@
+package cse213.final_project.Jannat;
+
+public class ApplyForNewNid
+{
+    @javafx.fxml.FXML
+    private TextField phoneNumberTextField;
+    @javafx.fxml.FXML
+    private TextField addressTextField;
+    @javafx.fxml.FXML
+    private TextField emailTextField;
+    @javafx.fxml.FXML
+    private TextField birthCertificateNoTextField;
+    @javafx.fxml.FXML
+    private TextField citizenIdTextField;
+    @javafx.fxml.FXML
+    private TextField nameTextField;
+    @javafx.fxml.FXML
+    private DatePicker dateOfBirthDatePicker;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/cse213/final_project/Jannat/Citizen.java
@@ -0,0 +1,15 @@
+package cse213.final_project.Jannat;
+
+import javafx.scene.control.DatePicker;
+
+public class Citizen {private String citizenID;
+    private String name;
+    private String address;
+    private DatePicker dateOfBirth;
+    private Long birthCertificate;
+    private String email;
+    private Integer phoneNumber;
+
+
+
+}
