90bbcb9a9bd757a07f090e80df76424ff8355a37
Jannatul Fardous <Jannatulferdousmobi>
2025-04-16 17:05:27 +0600

    goal2,3,4 ,5added

--- src/main/java/cse213/final_project/Jannat/Goal5DuplicateNID.java
+++
@@ -1,7 +0,0 @@
-package cse213.final_project.Jannat;
-
-public class Goal5DuplicateNID
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
---
+++ src/main/java/cse213/final_project/Jannat/Goal5duplicateNidController.java
@@ -0,0 +1,37 @@
+package cse213.final_project.Jannat;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class Goal5duplicateNidController
+{
+    @javafx.fxml.FXML
+    private ComboBox nidStatusComboBox;
+    @javafx.fxml.FXML
+    private TextField addressTextField;
+    @javafx.fxml.FXML
+    private TextField nidNumberTextField;
+    @javafx.fxml.FXML
+    private TextField fullNameTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @Deprecated
+    public void navigateToDuplicateNIDForm(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleFileUploadButtonAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitForVerificationButtonAction(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleLoginButtonAction(ActionEvent actionEvent) {
+    }
+}
