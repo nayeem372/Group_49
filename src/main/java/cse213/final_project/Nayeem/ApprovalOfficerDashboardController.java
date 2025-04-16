1c70c01870c62b81a50f71c74fce850f0ed19f99
nayeem372 <hnayeem372@gmail.com>
2025-04-16 15:13:16 +0600

    ApprovalOfficerDashboard.fxml

---
+++ src/main/java/cse213/final_project/Nayeem/ApprovalOfficerDashboardController.java
@@ -0,0 +1,74 @@
+package cse213.final_project.Nayeem;
+
+import javafx.event.ActionEvent;
+
+public class ApprovalOfficerDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @Deprecated
+    public void handleMaintainApprovalLogs(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleFollowUpPendingApplications(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleApproveFinalNID(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleReviewFlaggedCases(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleCoordinateUnclearCases(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleHandleAppeals(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleEnsureApplicantInfoCorrect(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void handleProvidePrintingAuthorization(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void coordinateUnclearCasesOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reviewFlaggedCasesOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void maintainApprovalLogsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approveFinalNIDOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void providePrintingAuthorizationOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void appealsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ensureApplicantInfoCorrectOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void followUpPendingApplicationsOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/cse213/final_project/Nayeem/ApproveFinalNIDController.java
@@ -0,0 +1,37 @@
+package cse213.final_project.Nayeem;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class ApproveFinalNIDController
+{
+    @javafx.fxml.FXML
+    private TextField flaggedIssuesField;
+    @javafx.fxml.FXML
+    private CheckBox identityCheckBox;
+    @javafx.fxml.FXML
+    private TextField verificationStatusField;
+    @javafx.fxml.FXML
+    private TextField applicantDetailsField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void generateSummaryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notifyApplicantOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approveForPrintingOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void forwardToPrintingOnAction(ActionEvent actionEvent) {
+    }
+}
