1f965ed071a9c4bcc93b3f46e4452d0a8373feab
Jannatul Fardous <Jannatulferdousmobi>
2025-04-20 15:08:42 +0600

    login view and create class updated

--- src/main/java/cse213/final_project/Jannat/Goal7PayServiceController.java
+++ src/main/java/cse213/final_project/Jannat/Goal7PayServiceController.java
@@ -23,6 +23,21 @@ public class Goal7PayServiceController
 
     @javafx.fxml.FXML
     public void paymentButtonOnAction(ActionEvent actionEvent) {
+        if (creditCardCheckBox.isSelected()) {
+            String cardNumber = creditCardTextField.getText();
+
+            if (cardNumber.length() <= 10) {
+                String amount = amountTextField.getText();
+
+                confirmationMessageLabel.setText("Payment of " + amount + " Taka successful.");
+
+                System.out.println("Payment processed for card: " + cardNumber + " for amount: " + amount + " Taka.");
+            } else {
+                confirmationMessageLabel.setText("Invalid card number. It should be 10 digits or less.");
+            }
+        } else {
+            confirmationMessageLabel.setText("Please select the payment method.");
+        }
 
     }
 }
--- src/main/java/cse213/final_project/Jannat/NIDApplication.java
+++
@@ -1,8 +0,0 @@
-package cse213.final_project.Jannat;
-
-public class NIDApplication {
-    private String applicationID;
-    private String citizenID;
-    private String applicationDate;
-    private String status;
-}
